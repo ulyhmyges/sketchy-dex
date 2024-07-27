924037c249738d42f780fa9fb84ab1dc4b7dde42
ulyh <janeuly@pm.me>
2024-07-27 13:33:14 +0200

    Add docker-compose.yml

--- api/database/db.go
+++ api/database/db.go
@@ -3,14 +3,26 @@ package database
 import (
 	"context"
 	"fmt"
+	"log"
 	"os"
 
 	"github.com/jackc/pgx/v5"
 )
 
-func Connect() {
-	conn, err := pgx.Connect(context.Background(), os.Getenv("DATABSE_URL"))
+func Connect() *pgx.Conn {
+	conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
 	if err != nil {
-		fmt.Fprint(os.Stderr, "Unable to connect to database: %v\n", err)
+		fmt.Fprintf(os.Stderr, "===Unable to connect to database: %v\n===", err)
+		log.Fatal()
 	}
+	return conn
+}
+
+func Disconnect(conn *pgx.Conn) {
+	err := conn.Close(context.Background())
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to disconnect from database: %v\n", err)
+		return
+	}
+	log.Println("Disconnect from daatabase")
 }
--- api/main.go
+++ api/main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"api/database"
 	"api/model"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	pg := database.Connect()
+	defer database.Disconnect(pg)
 	router := gin.Default()
 	router.GET("/users", model.GetUsers)
 	router.POST("/users", model.PostUsers)
